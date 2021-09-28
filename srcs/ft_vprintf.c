170aed79c7fa8fd643f4171dbc650e2f44f06af7
Alex <jimenezs.alex@gmail.com>
2021-09-28 15:52:48 +0200

    Todo menos el %p

--- srcs/ft_vprintf.c
+++ srcs/ft_vprintf.c
@@ -6,7 +6,7 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 19:25:36 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/09/27 11:25:28 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/09/28 15:13:41 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ ssize_t	ft_vprintf(char c1, char c2, va_list ag)
 	else if (c1 == '%' && c2 == 'd')
 		trigger = ft_putnbr(va_arg(ag, int));
 	else if (c1 == '%' && c2 == 'u')
-		trigger = ft_putnbr(va_arg(ag, int));
+		trigger = ft_putnbr_u(va_arg(ag, int));
 	else if (c1 == '%' && c2 == 'x')
-		trigger = ft_putnbr(va_arg(ag, int));
+		trigger = ft_putstr(ft_itoh(va_arg(ag, unsigned int), 0));
 	else if (c1 == '%' && c2 == 'X')
-		trigger = ft_putnbr(va_arg(ag, int));
-	else if (c1 == '%' && c2 == 'p')
-		trigger = puntero;
+		trigger = ft_putstr(ft_itoh(va_arg(ag, unsigned int), 1));
+//	else if (c1 == '%' && c2 == 'p')
+		//trigger = puntero;
 	else
 		trigger = -1;
 	return (trigger);
