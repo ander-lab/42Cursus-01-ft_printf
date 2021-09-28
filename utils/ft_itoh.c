1f26a56a1a39132a2a06e3fd613e4edf12aaa97c
Alex <jimenezs.alex@gmail.com>
2021-09-28 15:54:12 +0200

    Corregido error dígitos

--- utils/ft_itoh.c
+++ utils/ft_itoh.c
@@ -6,13 +6,13 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/26 19:56:29 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/09/27 11:41:51 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/09/28 15:51:18 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static size_t	ft_size(int n)
+static size_t	ft_size(unsigned long int n)
 {
 	size_t	size;
 
@@ -32,7 +32,7 @@ char	*ft_itoh(unsigned long int n, int caps)
 	unsigned long int	aux;
 
 	size = ft_size(n);
-	hex = malloc(sizeof(char) * size + 1);
+	hex = malloc(sizeof(char) * size);
 	if (!hex)
 		return (0);
 	aux = 1;
@@ -40,7 +40,7 @@ char	*ft_itoh(unsigned long int n, int caps)
 	{
 		aux = n % 16;
 		if (aux < 10)
-			aux += 58;
+			aux += 48;
 		else if (aux > 9 && caps == 0)
 			aux += 87;
 		else if (aux > 9 && caps == 1)
@@ -49,7 +49,7 @@ char	*ft_itoh(unsigned long int n, int caps)
 		n /= 16;
 	}
 	if (n == 0)
-		hex[0] = '0';
+		hex[size - 1] = '0';
 	hex[aux] = '\0';
 	return (hex);
 }
