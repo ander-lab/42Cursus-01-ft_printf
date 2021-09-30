d8022f5c3cbd2f9a94df3eaf24d1fe1807dbdd94
Alex <jimenezs.alex@gmail.com>
2021-09-30 21:04:47 +0200

    new functions

--- srcs/ft_vprintf.c
+++ srcs/ft_vprintf.c
@@ -6,7 +6,7 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 19:25:36 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/09/30 11:43:41 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/09/30 21:00:57 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ ssize_t	ft_vprintf(char c1, char c2, va_list ag)
 	else if (c1 == '%' && c2 == 'd')
 		trigger = ft_putnbr(va_arg(ag, int));
 	else if (c1 == '%' && c2 == 'u')
-		trigger = ft_putnbr_u(va_arg(ag, int));
+		trigger = ft_putnbr_u(va_arg(ag, unsigned int));
 	else if (c1 == '%' && c2 == 'x')
 	{
 		aux = ft_itoh(va_arg(ag, unsigned int), 0);
@@ -41,8 +41,12 @@ ssize_t	ft_vprintf(char c1, char c2, va_list ag)
 		trigger = ft_putstr(aux);
 		free (aux);
 	}
-//	else if (c1 == '%' && c2 == 'p')
-		//trigger = puntero;
+	else if (c1 == '%' && c2 == 'p')
+	{
+		aux = ft_ptr(va_arg(ag, unsigned int *));
+		trigger = ft_putstr(aux);
+		free (aux);
+	}
 	else
 		trigger = -1;
 	return (trigger);
