aa27586eb2cfb9e525296988d0ca288aab143e9e
Alex <jimenezs.alex@gmail.com>
2021-09-30 21:04:12 +0200

    new functions

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 18:20:29 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/09/29 19:12:18 by Alejandro        ###   ########.fr       */
+/*   Updated: 2021/09/30 20:34:52 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,13 @@ ssize_t	ft_vprintf(char c1, char c2, va_list ag);
 /*
 ** UTILS
 */
+char	*ft_ptr(void *p);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 int		ft_putchar(char c);
-int		ft_putnbr(int nb);
-int		ft_putnbr_u(int nb);
+int	ft_putnbr(int numbr);
+//int		ft_putnbr_u(int nb);
+unsigned int	ft_putnbr_u(unsigned int nb);
+//unsigned int	ft_putnbr_u(unsigned int nb);
 int		ft_putstr(char *s);
 char	*ft_itoh(unsigned long int n, int caps);
 
