992e84000e9a6221cad43ba9aac1dbd528860f56
Alex <jimenezs.alex@gmail.com>
2021-09-30 16:09:25 +0200

    solved count

--- utils/ft_putnbr.c
+++ utils/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: ajimenez <ajimenez@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 19:48:03 by ajimenez          #+#    #+#             */
-/*   Updated: 2021/09/28 17:58:33 by ajimenez         ###   ########.fr       */
+/*   Updated: 2021/09/30 12:45:57 by ajimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,16 @@ int	ft_putnbr(int nb)
 	unsigned int	res;
 	int	r;
 
+	r = 0;
 	if (nb < 0)
 	{
-		ft_putchar('-');
+		r += ft_putchar('-');
 		res = (unsigned int)(nb * -1);
 	}
 	else
 		res = (unsigned int)nb;
 	if (res >= 10)
-		r = ft_putnbr(res / 10);
-	r = ft_putchar((char)(res % 10 + 48));
-	return (r + 1);
+		r += ft_putnbr(res / 10);
+	r += ft_putchar((char)(res % 10 + 48));
+	return (r);
 }
